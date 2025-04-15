7a1e06ea2c81ed1ae3d01f3bf5132c280da7a367
iiputilo <iiputilo@ya.ru>
2025-04-15 17:43:26 +0300

    Signal Generator v1.0. Spectrum and signal graphics now open in separate windows.

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <graphics_drawer.h>
+#include <signal_generator.h>
+
+int main() {
+    std::cout << "Choose the option:\n1. Generate signal\n2. Show graphics\n3. Exit program\n\nYour choice-> ";
+
+    int userChoice;
+    std::cin >> userChoice;
+
+    switch (userChoice) {
+        case 1: {
+            if (generate()) {
+                std::cout << "Signal generated successfully.\n";
+            } else {
+                std::cerr << "Error generating signal.\n";
+            }
+            break;
+        }
+        case 2: {
+            if (showGraphics()) {
+                std::cout << "Graphics displayed successfully.\n";
+            } else {
+                std::cerr << "Error displaying graphics.\n";
+            }
+            break;
+        }
+        case 3:
+            std::cout << "Exiting program...\n";
+            break;
+        default:
+            std::cerr << "Invalid choice. Please try again.\n";
+    }
+}
